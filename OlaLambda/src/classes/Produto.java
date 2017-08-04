ffd276999ee1fc4fc67566912f7e1a218c779717
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-04 00:58:27 -0300

    implementado classe

--- OlaLambda/src/classes/Produto.java
+++ OlaLambda/src/classes/Produto.java
@@ -1,5 +1,28 @@
 package classes;
 
 public class Produto {
+	
+	private String nome;
+	private int valor;
+	
+	public Produto(String nome, int valor) {
+		super();
+		this.nome = nome;
+		this.valor = valor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getValor() {
+		return valor;
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
 
 }
