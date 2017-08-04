491e58cefec2a56d74603ef4be0885b9367019d3
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-04 11:22:51 -0300

    feito teste com a classe carrinho

--- OlaLambda/src/classes/Principal.java
+++ OlaLambda/src/classes/Principal.java
@@ -15,6 +15,15 @@ public class Principal {
 		lista.forEach( p -> p.darDesconto(10));
 		
 		lista.forEach( p -> System.out.println(p) );
+		System.out.println("-------------");
+		
+		//Aqui utilizamod um carrinho para armazenar a lista de produtos
+		//veja que, existe uma interface funcional chamada Condicao
+		//a partir dela, aplicamos o lambda na função darDesconto
+		Carrinho c = new Carrinho(lista);
+		c.darDesconto(10, p -> p.getValor() > 100);
+		
+		c.getLista().forEach(System.out::println);
 
 		//Caso só precisemos chamar um método sem parâmetro
 		//podemos fazer da seguinte maneira
