b988f7ff172c709aa67eab2e7ff0296d38ed59f3
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-08 00:21:05 -0300

    classe para testar

---
+++ AcessoBD/src/main/Principal.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.List;
+
+import model.Usuario;
+import model.UsuarioDAO;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		List<Usuario> lista = UsuarioDAO.todosUsuarios();
+		
+		lista.forEach(System.out::println);
+
+	}
+
+}
