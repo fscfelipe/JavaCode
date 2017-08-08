fb5021c046ceea5cd8d13abc35f49d4b5e0c005c
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-08 12:41:00 -0300

    novo metodo de teste

--- AcessoBD/src/tests/TesteUsuarioDAO.java
+++ AcessoBD/src/tests/TesteUsuarioDAO.java
@@ -2,11 +2,12 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.dbunit.Assertion;
 import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
 import org.dbunit.util.fileloader.FlatXmlDataFileLoader;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +18,7 @@ import model.UsuarioDAO;
 public class TesteUsuarioDAO {
 
 	JdbcDatabaseTester jdt;
+	
 	
 	@Before
 	public void setUp() throws Exception {
@@ -34,6 +36,26 @@ public class TesteUsuarioDAO {
 		List<Usuario> lista = UsuarioDAO.todosUsuarios();
 		assertEquals(2, lista.size());
 		assertEquals("joao", lista.get(0).getLogin());
+	}
+	
+	@Test
+	public void testInsereNovo() throws Exception {
+		Usuario u = new Usuario();
+		u.setLogin("duda");
+		u.setNome("Maria Eduarda");
+		u.setEmail("duda@email.com.br");
+		
+		UsuarioDAO.inserirUsuario(u);
+		
+		IDataSet currentDataset = jdt.getConnection().createDataSet();
+		ITable currentTable = currentDataset.getTable("USUARIO");
+		
+		FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
+		IDataSet expectedDataset = loader.load("/verifica.xml");
+		ITable expectedTable = expectedDataset.getTable("USUARIO");
+		
+		Assertion.assertEquals(expectedTable, currentTable);
+
 	}
 
 }
