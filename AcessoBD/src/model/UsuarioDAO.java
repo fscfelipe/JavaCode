851b39a3e37a77564406bfab5c2a5bc66dc960a5
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-08 00:20:50 -0300

    implementado todo o acesso para o banco de usuarios

--- AcessoBD/src/model/UsuarioDAO.java
+++ AcessoBD/src/model/UsuarioDAO.java
@@ -2,6 +2,8 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +25,21 @@ public class UsuarioDAO {
 		// pelo mesmo.
 
 		try (Connection c = DriverManager.getConnection
-				("jdbc:postgresql://localhost/usuario", "postgres", "12345")) {
+				("jdbc:postgresql://localhost/usuarios", "postgres", "12345")) {
 
+			String sql = "SELECT login, nome, email FROM usuario";
 			
+			PreparedStatement stmt = c.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				Usuario u = new Usuario();
+				u.setLogin(rs.getString("login"));
+				u.setNome(rs.getString("nome"));
+				u.setEmail(rs.getString("email"));
+				
+				lista.add(u);
+			}
 			
 			
 		} catch (SQLException e) {
