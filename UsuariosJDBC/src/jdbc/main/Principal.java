d37434c82d23b1b8fb3dab25cc7c14bfcb93ffd3
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-21 06:05:14 -0300

    testes

--- UsuariosJDBC/src/jdbc/main/Principal.java
+++ UsuariosJDBC/src/jdbc/main/Principal.java
@@ -1,12 +1,30 @@
 package jdbc.main;
 
+import java.util.List;
+
 import jdbc.dao.UsuarioNoBancoDAO;
+import jdbc.model.Usuario;
 
 public class Principal {
 
 	public static void main(String[] args) {
 
 		UsuarioNoBancoDAO udao = new UsuarioNoBancoDAO();
+		
+		/*Usuario u = new Usuario();
+		u.setLogin("marina");
+		u.setEmail("marina@rmail.com");
+		u.setNome("Marina rina");
+		u.setSenha("rina");
+		u.setPontos(30);*/
+		
+		List<Usuario> lista = udao.ranking();
+		
+		for(Usuario u:lista) {
+			System.out.println("Nome : " + u.getNome() + " pontos: " + u.getPontos());
+		}
+		
+		
 	}
 
 }
