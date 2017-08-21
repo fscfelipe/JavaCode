54ce203588fe534b9375b585a6bf8c477f531eaa
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-20 23:05:45 -0300

    classe para testar o uso do banco

---
+++ UsuariosJDBC/src/jdbc/main/Principal.java
@@ -0,0 +1,12 @@
+package jdbc.main;
+
+import jdbc.dao.UsuarioNoBancoDAO;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		UsuarioNoBancoDAO udao = new UsuarioNoBancoDAO();
+	}
+
+}
