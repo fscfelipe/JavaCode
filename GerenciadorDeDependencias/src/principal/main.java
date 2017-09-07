023d8799f054d4e9d6f16507e058d2627b19d92c
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-09-07 19:53:25 -0300

    projeto para testar gerenciadores de dependências

---
+++ GerenciadorDeDependencias/src/principal/main.java
@@ -0,0 +1,12 @@
+package principal;
+
+import org.dbunit.assertion.DbUnitAssert;
+
+public class main {
+
+	public static void main(String[] args) {
+		DbUnitAssert s = new DbUnitAssert();
+
+	}
+
+}
